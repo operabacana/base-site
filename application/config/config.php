c71032ff5172196a8ecfda608c99aded96d351ab
operabacana <leao@operabacana.com.br>
2013-01-24 17:42:01 -0200

    Primeiras alterações Leão

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/base-site/';
+$config['base_url']	= 'http://localhost:8080/base-site/';
 
 /*
 |--------------------------------------------------------------------------
