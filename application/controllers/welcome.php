0595248d0d378d3c70e4c5afeb5ad017f3b3dba1
operabacana <leao@operabacana.com.br>
2013-01-24 17:42:01 -0200

    Primeiras alterações Leão

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -1,22 +1,22 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-require APPPATH.'/libraries/REST_Controller.php';
+//require APPPATH.'/libraries/REST_Controller.php';
 
-class Welcome extends REST_Controller {
+class Welcome extends CI_Controller {
 
 	function __construct()
 	{
 		parent::__construct();
 	}
 
-	function index_get()
+	function index()
 	{
-        $this -> response($this->get('nome'), 200);
+        //$this -> response($this->get('nome'), 200);
 	}
 
-	function teste_get($id)
+	function teste($id)
 	{
-       $this -> response(array('id'=>$id) , 200);
+       //$this -> response(array('id'=>$id) , 200);
 	}
 }
 
