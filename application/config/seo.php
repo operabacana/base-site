bfe32cb86ebdf5e64ebdc4ddc9e3523a831544a5
Demetrius F. Campos <demetrius_nf@hotmail.com>
2013-01-29 11:31:14 -0200

    Adicionado Views, Controllers, Helpers e configs
    Foram adicionadas as principais views, controllers e helpers de um site
    padrão. Algumas configurações também foram feitas.

--- application/config/seo.php
+++ application/config/seo.php
@@ -1,6 +1,11 @@
 <?php
 
-$config[""] = "";
-
+$config["quem_somos"] = "Quem somos";
+$config["infra_estrutura"] = "Infra-estrutura";
+$config["porque_escolher"] = "Porque escolher a Time";
+$config["fale_conosco"] = "Fale conosco";
+$config["filiais"] = "Filiais";
+$config["cotacao_online"] = "Cotação online";
+$config["trabalhe_conosco"] = "Trabalhe conosco";
 
 ?>
