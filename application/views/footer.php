d4a45b5f71e69df1d017be11d2ccf962bddf26f5
Jonathan <jougabetta@gmail.com>
2013-03-25 14:11:57 -0300

    retirando prefixfree

--- application/views/footer.php
+++ application/views/footer.php
@@ -9,7 +9,6 @@
 
 ?>
 <script language="javascript" type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" ></script>
-<script language="javascript" type="text/javascript" src="https://raw.github.com/LeaVerou/prefixfree/gh-pages/plugins/prefixfree.dynamic-dom.min.js" ></script>
 <script language="javascript" type="text/javascript" src="http://ajax.microsoft.com/ajax/jquery.validate/1.7/jquery.validate.min.js" ></script>
 <script language="javascript" type="text/javascript" src="<?php echo $jsMain; ?>" ></script>
 
